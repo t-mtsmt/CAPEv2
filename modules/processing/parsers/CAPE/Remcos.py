0d205270b23397d1365e6c7d7de7b3ce55e73b82
Takashi Matsumoto <77025706+t-mtsmt@users.noreply.github.com>
2023-12-24 03:24:49 +0900

    Supported if you have multiple C2 servers

--- modules/processing/parsers/CAPE/Remcos.py
+++ modules/processing/parsers/CAPE/Remcos.py
@@ -215,9 +215,11 @@ def extract_config(filebuf):
                         p_data[idx_list[i]] = reaction_list[int(chr(cont[0]))]
                 elif i == 0:
                     # various separators have been observed
-                    separator = next((x for x in (b"|", b"\x1e", b"\xff\xff\xff\xff") if x in cont))
-                    host, port, password = cont.split(separator, 1)[0].split(b":")
-                    p_data["Control"] = f"tcp://{host.decode()}:{port.decode()}:{password.decode('utf8', 'backslashreplace')}"
+                    separator = next((x for x in (b"\n", b"|", b"\x1e", b"\xff\xff\xff\xff") if x in cont))
+                    p_data["Control"] = []
+                    for c2 in cont.strip(separator).split(separator):
+                        host, port, password = c2.split(b":")
+                        p_data["Control"].append(f"tcp://{host.decode()}:{port.decode()}:{password.decode('utf8', 'backslashreplace')}")
                 else:
                     p_data[idx_list[i]] = cont
 
